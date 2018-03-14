7ab101c02e0da0be48a4f59527b85d5c42387d8b
guptaco@ymail.com <guptaco@ymail.com>
2018-03-14 02:20:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,20 +10,16 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- var x = parseInt(Math.floor(Math.random() * Math.floor(100)))
- item = { itemName: item, itemPrice: x}
- cart.push(item)
- console.log (item.itemName + 'has been added to your cart.')
-}
+
 }
 
 function viewCart() {
   if (cart.length === 0) {
-    return 'Your shopping cart is empty.'
+    return 'Your shopping cart is empty.';
   }
   else{
   for (var i = 0, i < cart.length, i++){
-    return cart[i]
+    return cart[i];
   }
   } 
 }
